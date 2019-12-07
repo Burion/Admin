0e71b1a1f41459721df20566f5528b6900938c9f
Vlad Buryak <vladislavburyak00@gmail.com>
2019-12-07 14:37:49 +0200

    [add] order models for presentation layer

--- Musical_WebStore_BlazorApp/Shared/AddOrderModel.cs
+++ Musical_WebStore_BlazorApp/Shared/AddOrderModel.cs
@@ -5,9 +5,9 @@ namespace Admin.ViewModels
     public class AddOrderModel
     {
         public int ServiceId {get;set;}
-        public int UserId {get;set;}
+        public string UserId {get;set;}
         public string Text {get;set;}
-        public int OrderTypeId {get;set;}
-        public int DeviceId {get;set;}
+        public string OrderTypeId {get;set;}
+        public string DeviceId {get;set;}
     }
 }
