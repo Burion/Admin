cf7657e3ff6908c232035655175010329471e194
Vlad Buryak <vladislavburyak00@gmail.com>
2019-12-17 16:17:45 +0200

    [fix] chat model

--- Musical_WebStore_BlazorApp/Shared/ChatModel.cs
+++ Musical_WebStore_BlazorApp/Shared/ChatModel.cs
@@ -5,9 +5,11 @@ namespace Admin.ViewModels
 {
     public class ChatModel
     {
+        public int Id {get;set;}
         public string Name {get;set;}
         public string Description {get;set;}
-        public List<string> ChatUsersNames {get;set;}
+        public int CompanyId {get;set;}
+        public CompanyModel Company {get;set;}
     }
     
 }
