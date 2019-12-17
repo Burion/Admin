5f65be973acd0cb7d99afd6357eacfd53f94ff5b
Vlad Buryak <vladislavburyak00@gmail.com>
2019-12-17 16:19:23 +0200

    [add] addworkertoordermodel model

---
+++ Musical_WebStore_BlazorApp/Shared/AddWorkerToOrderModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Admin.ViewModels
+{
+    public class AddWorkerToOrderModel
+    {
+        public int OrderId {get;set;}
+        public string UserId {get;set;}
+    }
+}
