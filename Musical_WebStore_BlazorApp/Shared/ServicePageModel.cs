80a9e61373846bc6c9fc3aac2e758a6285812958
Vlad Buryak <vladislavburyak00@gmail.com>
2019-12-17 22:12:22 +0200

    [add] service page and model

---
+++ Musical_WebStore_BlazorApp/Shared/ServicePageModel.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Admin.ViewModels
+{
+    public class ServicePageModel
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Address {get;set;}
+        public string Phone {get;set;}
+        public int OrdersCount {get;set;}
+        public int OrdersSucceedCount {get;set;}
+        public int ReviewsCount {get;set;}
+        public float AvarageMark {get;set;}
+
+    }
+}
