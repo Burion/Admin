b6f6c21edd237e134aaca4a6ee54309dc8bbfe6a
Vlad Buryak <vladislavburyak00@gmail.com>
2019-12-17 16:17:19 +0200

    [fix] chat entity

--- Musical_WebStore_BlazorApp/Server/Models/Chat.cs
+++ Musical_WebStore_BlazorApp/Server/Models/Chat.cs
@@ -8,7 +8,10 @@ namespace Admin.Models
     public int Id {get;set;}
     public string Name {get;set;}
     public string Description {get;set;}
-
+    public int ServiceId {get;set;}
+    public virtual Service Service {get;set;}
+    public int CompanyId {get;set;}
+    public virtual Company Company {get;set;}
     public virtual ICollection<ChatUser> ChatUsers {get;set;}
     }
 
